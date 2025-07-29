d94767b4ca772014f56a979d55bc0be8014cb7c5
Diego Viana <dsantos@inf.ufsm.br>
2025-07-29 02:20:12 -0300

    Update post.tsx

--- src/components/posts-section/post.tsx
+++ src/components/posts-section/post.tsx
@@ -7,9 +7,7 @@ export function Post(post: P) {
       <h3 className="bg-blue-700 px-4 pt-4 pb-4 text-base font-semibold">
         {post.title}
       </h3>
-      <p className="line-clamp-5 px-4 pt-4 text-sm text-zinc-200">
-        {post.body}
-      </p>
+      <p className="line-clamp-5 px-4 pt-4 text-sm">{post.body}</p>
       <p className="px-4 py-4 text-xs font-semibold">{post.tags}</p>
       <Link
         className="mx-4 mb-4 w-fit place-self-end rounded-sm bg-gray-900 px-2 py-1 text-sm duration-300 hover:bg-gray-950"
