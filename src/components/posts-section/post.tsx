03ba3a880ae15dd8f3bbfac42beb0fbaefb6b58e
Diego Viana <dsantos@inf.ufsm.br>
2025-07-29 03:02:02 -0300

    Update post.tsx

--- src/components/posts-section/post.tsx
+++ src/components/posts-section/post.tsx
@@ -4,11 +4,13 @@ import Link from 'next/link'
 export function Post(post: P) {
   return (
     <div className="flex flex-col justify-between overflow-hidden rounded-lg bg-gray-800">
-      <h3 className="bg-blue-700 px-4 pt-4 pb-4 text-base font-semibold">
-        {post.title}
-      </h3>
+      <div className="bg-blue-700 px-4 py-4">
+        <h3 className="line-clamp-2 text-base font-semibold">{post.title}</h3>
+      </div>
       <p className="line-clamp-5 px-4 pt-4 text-sm">{post.body}</p>
-      <p className="px-4 py-4 text-xs font-semibold">{post.tags}</p>
+      <p className="line-clamp-2 px-4 py-4 text-xs font-semibold">
+        {post.tags}
+      </p>
       <Link
         className="mx-4 mb-4 w-fit place-self-end rounded-sm bg-gray-900 px-2 py-1 text-sm duration-300 hover:bg-gray-950"
         href={`/posts/${post.slug}`}
