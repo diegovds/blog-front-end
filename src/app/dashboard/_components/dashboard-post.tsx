e2868efd419dc23cbcd0278c25fb690b646467a2
Diego Viana <dsantos@inf.ufsm.br>
2025-07-30 21:18:07 -0300

    Update dashboard-post.tsx

--- src/app/dashboard/_components/dashboard-post.tsx
+++ src/app/dashboard/_components/dashboard-post.tsx
@@ -9,16 +9,20 @@ interface DashboardPostProps {
 
 export function DashboardPost({ post, token }: DashboardPostProps) {
   return (
-    <div className="flex flex-col justify-between overflow-hidden rounded-lg bg-gray-800">
+    <div className="flex min-h-[256px] flex-col overflow-hidden rounded-lg bg-gray-800">
       <div className="bg-blue-700 px-4 py-4">
-        <h3 className="line-clamp-2 text-base font-semibold text-balance">
+        <h3 className="line-clamp-1 text-base font-semibold text-balance">
           {post.title}
         </h3>
       </div>
-      <p className="line-clamp-5 px-4 pt-4 text-sm text-balance">{post.body}</p>
-      <p className="line-clamp-2 px-4 py-4 text-xs font-semibold text-balance">
-        {post.tags}
-      </p>
+      <div className="flex flex-1 flex-col justify-between">
+        <p className="line-clamp-5 px-4 pt-4 text-sm text-balance">
+          {post.body}
+        </p>
+        <p className="line-clamp-2 px-4 py-4 text-xs font-semibold text-balance">
+          {post.tags}
+        </p>
+      </div>
       <div className="mx-4 mb-4 flex items-center justify-between">
         <div className="flex gap-2">
           <p
