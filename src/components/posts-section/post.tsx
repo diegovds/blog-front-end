2d39b9c3c0ad94ab19fb37514f30d1eaa5517f70
Diego Viana <dsantos@inf.ufsm.br>
2025-07-30 21:21:22 -0300

    Update post.tsx

--- src/components/posts-section/post.tsx
+++ src/components/posts-section/post.tsx
@@ -3,16 +3,20 @@ import Link from 'next/link'
 
 export function Post(post: P) {
   return (
-    <div className="flex flex-col justify-between overflow-hidden rounded-lg bg-gray-800">
+    <div className="flex min-h-[256px] flex-col overflow-hidden rounded-lg bg-gray-800">
       <div className="bg-blue-700 px-4 py-4">
-        <h3 className="line-clamp-2 text-base font-semibold text-balance">
+        <h3 className="line-clamp-1 text-base font-semibold text-balance">
           {post.title}
         </h3>
       </div>
-      <p className="line-clamp-5 px-4 pt-4 text-sm text-balance">{post.body}</p>
-      <p className="line-clamp-2 px-4 py-4 text-xs font-semibold text-balance">
-        {post.tags}
-      </p>
+      <div className="flex flex-1 flex-col justify-between">
+        <p className="line-clamp-5 px-4 pt-4 text-sm text-balance">
+          {post.body}
+        </p>
+        <p className="line-clamp-2 px-4 py-4 text-xs font-semibold text-balance">
+          {post.tags}
+        </p>
+      </div>
       <Link
         className="mx-4 mb-4 w-fit place-self-end rounded-sm bg-gray-900 px-2 py-1 text-xs duration-300 hover:bg-gray-950 md:text-sm"
         href={`/posts/${post.slug}`}
